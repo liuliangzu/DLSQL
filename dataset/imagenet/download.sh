ae6f6de514bc4e2c82961b264313dd0bf0e84fe7
刘良祖 <110663967+liuliangzu@users.noreply.github.com>
2024-05-01 17:06:10 +0800

    Update download.sh

--- dataset/imagenet/download.sh
+++ dataset/imagenet/download.sh
@@ -1,1 +1,14 @@
+https://www.image-net.org/
 
+mkdir train
+mkdir val
+tar xvf ILSVRC2012_img_train.tar -C ./train
+tar xvf ILSVRC2012_img_val.tar -C ./val
+
+dir=./train 
+for x in `ls $dir/*tar` do     
+  filename=`basename $x .tar`     
+  mkdir $dir/$filename     
+  tar -xvf $x -C $dir/$filename 
+done 
+rm *.tar
